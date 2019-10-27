f2ee32b47771cbf589ec720e3f4670cc7b0a255a
Andrzej <xurten@gmail.com>
2019-10-27 13:11:10 +0100

    Created class library project

---
+++ ClassLibrary/Pages/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace ClassLibrary.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+    }
+}
---
+++ ClassLibrary/Pages/LoginPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace ClassLibrary.Pages
+{
+    public class LoginPage : BasePage
+    {
+        private const string LoginPageUrl = "https://the-internet.herokuapp.com/login";
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            driver.Url = LoginPageUrl;
+        }
+    }
+}
