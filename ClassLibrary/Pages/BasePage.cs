40e47cc81f57fe5ec6009c9888e3c3e07b3af92c
Andrzej <xurten@gmail.com>
2019-10-27 13:47:45 +0100

    Added LoginPage test

--- ClassLibrary/Pages/BasePage.cs
+++ ClassLibrary/Pages/BasePage.cs
@@ -1,14 +1,18 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace ClassLibrary.Pages
 {
     public class BasePage
     {
         protected IWebDriver driver;
+        protected WebDriverWait wait;
 
         public BasePage(IWebDriver driver)
         {
             this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
         }
 
     }
