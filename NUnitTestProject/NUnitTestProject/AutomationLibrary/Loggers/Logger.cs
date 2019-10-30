531af57381d2ec9a66be7a9e75fbb9d4f28b0ed5
Andrzej <xurten@gmail.com>
2019-10-30 22:47:04 +0100

    Added log4net

---
+++ NUnitTestProject/NUnitTestProject/AutomationLibrary/Loggers/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace NUnitTestProject.AutomationLibrary.Loggers
+{
+    public class Logger
+    {
+        public static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Logger()
+        {
+            var log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+            var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+        }
+
+        public void LogInfo(string message)
+        {
+            Log.Info(message);
+        }
+
+        public void LogFail(string message, Exception exception)
+        {
+            Log.Error(message, exception);
+        }
+    }
+}
--- NUnitTestProject/NUnitTestProject/AutomationLibrary/Pages/BasePage.cs
+++ NUnitTestProject/NUnitTestProject/AutomationLibrary/Pages/BasePage.cs
@@ -1,7 +1,9 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using log4net;
 using NUnitTestProject.AutomationLibrary.Helpers;
+using NUnitTestProject.AutomationLibrary.Loggers;
 
 namespace NUnitTestProject.Pages
 {
@@ -9,22 +11,26 @@ namespace NUnitTestProject.Pages
     {
         protected IWebDriver driver;
         protected WebDriverWait wait;
+        protected Logger logger;
 
         public BasePage(IWebDriver driver)
         {
             this.driver = driver;
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            logger = new Logger();
         }
 
         public BasePage Click(By selector, int timeout = 30000)
         {
             MouseHelper.Click(driver, selector, timeout);
+            logger.LogInfo($"Click event with selector {selector}");
             return this;
         }
 
         public BasePage SendKeys(By selector, string message)
         {
             KeyboardHelper.SendKeys(driver, selector, message);
+            logger.LogInfo($"Click event with selector {selector}");
             return this;
         }
     }   
